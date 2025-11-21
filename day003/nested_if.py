b5cc09a8d0f817708dba2c8cc7864dffeb6c3aed
Bas de Bolster <bas.de.bolster@gmail.com>
2025-11-21 15:01:47 +0100

    More nesting

--- day003/nested_if.py
+++ day003/nested_if.py
@@ -1,11 +1,22 @@
 height = int(input("Enter your height in cm: "))
+bill = 0
+
 if height >= 120:
     age = int(input("Enter your age: "))
     if age <= 12:
-        print("Youth ticket: $5")
+        print("Child tickets are: $5")
+        bill = 5
     elif age <= 18:
-        print("Teen ticket: $7")
+        print("Teen tickets are: $7")
+        bill = 7
     else:
-        print("Adult ticket: $12")
+        print("Adult tickets are: $12")
+
+    wants_photo = input("Do you want a photo taken? Y or N. ")
+    if wants_photo == "Y":
+        # Add $3 to their bill
+        bill += 3
+
+    print(f"Your final bill is: ${bill}")
 else:
     print("Sorry, you are not tall enough to ride.")
